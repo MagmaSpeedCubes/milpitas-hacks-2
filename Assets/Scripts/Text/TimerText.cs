0ca910449fa57fb2c3b4b80107bcb3b91ef810a6
MagmaSpeedCubes <110320002+MagmaSpeedCubes@users.noreply.github.com>
2025-05-24 17:33:35 -0700

    did some stuff

--- Assets/Scripts/Text/TimerText.cs
+++ Assets/Scripts/Text/TimerText.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 using TMPro;
+using System;
 
 public class TimerText : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI timerText;
 
     void Update(){
-        timerText.text = "Time: " + Timer.getTime() + " seconds";
+        timerText.text = "Time: " + Math.Round(Timer.getTime(), 1) + " seconds";
     }
 }
